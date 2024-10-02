f1f0d894ae4ddfc2b6cc29d6d50fa55f98fad99a
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2024-10-02 19:44:15 +0800

    fix

--- event/src/main/java/xyz/tcbuildmc/common/powerfullib/event/v0/api/listener/IListenerEntry.java
+++ event/src/main/java/xyz/tcbuildmc/common/powerfullib/event/v0/api/listener/IListenerEntry.java
@@ -1,11 +1,11 @@
 package xyz.tcbuildmc.common.powerfullib.event.v0.api.listener;
 
-import xyz.tcbuildmc.common.powerfullib.event.v0.impl.priority.EventPriorityImpl;
+import xyz.tcbuildmc.common.powerfullib.event.v0.api.priority.IEventPriority;
 
 public interface IListenerEntry<T> {
     T getListener();
 
-    EventPriorityImpl getPriority();
+    IEventPriority getPriority();
 
     static int sort(IListenerEntry<?> e1, IListenerEntry<?> e2) {
         return e1.getPriority().compareTo(e2.getPriority());
